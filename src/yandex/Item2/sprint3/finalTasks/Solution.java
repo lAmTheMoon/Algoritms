d101a013cb89fd9c236a33014206fa788ce4a555
Касатикова Екатерина Сергеевна <ESKasatikova@sberbank.ru>
2023-06-24 02:39:15 +0300

    add hw

--- src/yandex/Item2/sprint3/finalTasks/Solution.java
+++ src/yandex/Item2/sprint3/finalTasks/Solution.java
@@ -1,5 +1,12 @@
 package yandex.Item2.sprint3.finalTasks;
 
+/**
+ * ID посылки 88496623
+ * Алгоритм поиска индекса в сломанном массиве представляет собой модификацию бинарного поиска.
+ * Мы перемещаем указатели до тех пор, пока среднее значение границ не является индексом искомого значения.
+ * При отсутствии искомого значения в массиве возвращаем -1.
+ * В худшем случае потребуется log n сравнений, те скорость работы алгоритма O(log n)
+ */
 public class Solution {
 
     public static int brokenSearch(int[] arr, int k) {
@@ -7,7 +14,7 @@ public class Solution {
         int right = arr.length - 1;
         int middle = 0;
         while (left <= right) {
-            middle = (left + right)/ 2;
+            middle = (left + right) / 2;
             if (arr[middle] == k) {
                 return middle;
             }
@@ -17,12 +24,12 @@ public class Solution {
                 } else {
                     left = middle + 1;
                 }
-            } else{
-            if (arr[middle] < k && k <= arr[right]) {
-                left = middle + 1;
             } else {
-                right = middle - 1;
-            }
+                if (arr[middle] < k && k <= arr[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
             }
         }
         return -1;
