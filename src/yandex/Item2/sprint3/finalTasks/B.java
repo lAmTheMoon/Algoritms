4ccb059b75dc8cc391744a523702ce897315a64a
Ekaterina Kasatikova <KatyKat2010@yandex.ru>
2023-06-23 21:48:44 +0300

    add hw

---
+++ src/yandex/Item2/sprint3/finalTasks/B.java
@@ -0,0 +1,8 @@
+package yandex.Item2.sprint3.finalTasks;
+
+public class B {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/yandex/Item2/sprint3/finalTasks/Solution.java
@@ -0,0 +1,14 @@
+package yandex.Item2.sprint3.finalTasks;
+
+public class Solution {
+    
+    public static int brokenSearch(int[] arr, int k) {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+    }
+
+    private static void test() {
+        int[] arr = {19, 21, 100, 101, 1, 4, 5, 7, 12};
+        assert 6 == brokenSearch(arr, 5);
+    }
+}
